3c8e54e9e7d974dccb55a06bf991d3425a42d721
Jaja Chang <chihchia.chang@connect.qut.edu.au>
2023-05-12 00:21:22 +1000

    Push issue fixed

--- amplify/backend/function/decisionmakingW001/src/index.js
+++ amplify/backend/function/decisionmakingW001/src/index.js
@@ -4,7 +4,7 @@
 //  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
 //  */
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
+    console.log(event);
     const customerId = event.pathParameters.customerId;
     const customer = {'customerId': customerId, 'customerName': "customer " + customerId};
     const response = {
